cddd7d1b019573fbd8a2c4dd1c83464e4b53cdc6
Hansol Choi <76760480+hsc3@users.noreply.github.com>
2022-06-20 12:07:55 +0900

    Delete 구현 & 문자열 directory

--- 구현 & 문자열/[2587] 대표값2.py
+++
@@ -1,5 +0,0 @@
-# [2587] 대표값2 
-# Bronze 2 >> 구현, 수학
-
-numbers = sorted([int(input()) for _ in range(5)])
-print(sum(numbers)//5, numbers[2])
--- 구현 & 문자열/[2908] 상수.py
+++
@@ -1,8 +0,0 @@
-# [2908] 상수
-# Bronze 2 >> 수학, 구현
-
-import sys
-input = sys.stdin.readline
-
-A, B = input().split()
-print(A[::-1] if A[::-1] > B[::-1] else B[::-1]) 
